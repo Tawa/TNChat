e60f8c6980f6b41f44fa10074b0e14c0819cb1c1
Tawa Nicolas <tawanicolas@gmail.com>
2017-10-22 01:33:41 +0300

    Add Icon. Add Conversations to home screen.

--- iOS/TNChat/ViewControllers/ChatsViewController.swift
+++ iOS/TNChat/ViewControllers/ChatsViewController.swift
@@ -15,33 +15,57 @@ extension ChatConversation {
 }
 
 class ChatsViewController: UITableViewController {
+	
+	var conversations: [ChatConversation] {
+		return ConversationsManager.shared.conversations
+	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+		
+		ConversationsManager.shared.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		ConversationsManager.shared.reloadData()
+		tableView.reloadData()
+	}
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return conversations.count
     }
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let row = indexPath.row
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "conversationCell")!
+		
+		let conversation = conversations[row]
+		if let friendID = conversation.friendID {
+			let contact = ContactsManager.shared.getContact(withPhoneNumber: friendID)
+			cell.textLabel?.text = contact.name ?? "User"
+			cell.detailTextLabel?.text = conversation.message ?? "Message"
+		} else {
+			cell.textLabel?.text = "User"
+			cell.detailTextLabel?.text = "Message"
+		}
+		
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 56
+	}
 	
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		if segue.identifier == SegueIdentifiers.showContactsList {
@@ -52,10 +76,26 @@ class ChatsViewController: UITableViewController {
 				}
 			}
 		} else if segue.identifier == SegueIdentifiers.showConversation {
-			if let vc = segue.destination as? ConversationViewController, let contact = sender as? Contact {
-				vc.contact = contact
+			if let vc = segue.destination as? ConversationViewController {
+				if let contact = sender as? Contact {
+					vc.contact = contact
+				} else if let cell = sender as? UITableViewCell,
+					let indexPath = tableView.indexPath(for: cell),
+					let friendID = conversations[indexPath.row].friendID {
+					let contact = ContactsManager.shared.getContact(withPhoneNumber: friendID)
+					vc.contact = contact
+				}
 			}
 		}
 	}
+}
+
+extension ChatsViewController: ConversationsManagerDelegate {
+	func updatedConversation(forUserId userID: String) {
+		tableView.reloadData()
+	}
 	
+	func addedConversation(forUserId userID: String) {
+		tableView.reloadData()
+	}
 }
